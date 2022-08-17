0605e523f6d7740b7cec6903a99b1b5c563c4911
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2022-08-17 10:52:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,24 @@
 import streamlit as st
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
+# # Using object notation
+# add_selectbox = st.sidebar.selectbox(
+#     "How would you like to be contacted?",
+#     ("Email", "Home phone", "Mobile phone")
+# )
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
+# # Using "with" notation
+# with st.sidebar:
+#     add_radio = st.radio(
+#         "Choose a shipping method",
+#         ("Standard (5-15 days)", "Express (2-5 days)")
+#     )
+
+st.success('This is a success message!', icon:"✅")
+
+st.info('This is a purely informational message', icon="ℹ️")
+
+st.error('This is an error', icon="🚨")
+
+st.warning('This is a warning', icon="⚠️")
+
+
